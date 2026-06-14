7e653675ed7ac2ee7fb16f3c184a09c5ecd1e79a
Dim-CGP <gerber.dimitri@gmail.com>
2026-06-14 09:54:00 +0200

    Fix redirect 301 www vers non-www

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.cabinet-patrimonium.fr' }],
+        destination: 'https://cabinet-patrimonium.fr/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
